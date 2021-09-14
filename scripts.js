fee224cee90421f823733fcf293dce4a6a602ba3
Rodrigo Molina <rodrigomd94@gmail.com>
2021-09-13 22:14:17 -0600

    changed ipfs gateway

--- scripts.js
+++ scripts.js
@@ -24,8 +24,8 @@ fetch("https://cardaworlds-api.herokuapp.com/CheckAsset/" + nft_id, {
             "description": metadata.description,
             "name": metadata.name,
             "imageURL": imageURL,
-            "heightmap":"https://ipfs.io/ipfs/" + metadata.files[0].src.replace("ipfs://", ""),
-            "background":"https://ipfs.io/ipfs/" + metadata.files[1].src.replace("ipfs://", "")
+            "heightmap": "https://gw2.easy-ipfs.com/ipfs/" + metadata.files[0].src.replace("ipfs://", ""),
+            "background": "https://gw2.easy-ipfs.com/ipfs/" + metadata.files[1].src.replace("ipfs://", "")
         }
         init(nft_info.imageURL, nft_info.heightmap, nft_info.background, nft_info.name);
         animate();
@@ -38,7 +38,18 @@ fetch("https://cardaworlds-api.herokuapp.com/CheckAsset/" + nft_id, {
 
 
 function init(imageURL, heightmap, background, name) {
-    document.body.setAttribute('background', "url("+background+") no-repeat center center")
+
+    image.crossOrigin = "anonymous";
+    image.src = imageURL;
+    image.width = 720;
+    image.height = 1080;
+
+    height_image.crossOrigin = "anonymous";
+    height_image.src = heightmap;
+    height_image.width = 720;
+    height_image.height = 1080;
+
+    document.body.setAttribute('background', "url(" + background + ") no-repeat center center")
     info = document.createElement('div');
     info.style.position = 'absolute';
     info.style.top = '30px';
@@ -92,7 +103,8 @@ function init(imageURL, heightmap, background, name) {
     var ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
     scene.add(ambientLight);
 
-
+    const loader = new THREE.TextureLoader();
+    THREE.ImageUtils.crossOrigin = 'anonymous';
     var material = new THREE.MeshPhongMaterial({
         map: THREE.ImageUtils.loadTexture(imageURL),
         bumpMap: THREE.ImageUtils.loadTexture(heightmap),
@@ -147,12 +159,4 @@ function render() {
     renderer.render(scene, camera);
 
 }
-/* image.crossOrigin = "anonymous";
-image.src = './CardaWorld1106.png';
-image.width = 720;
-image.height = 1080;
 
-height_image.crossOrigin = "anonymous";
-height_image.src = 'https://i.ibb.co/kS3qWBL/heightmap-9.png';
-height_image.width = 720;
-height_image.height = 1080; */
